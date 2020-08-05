dcd2a0ff163e6c84f844c0e0da97ba63560050b4
Anish <atechchatterjeee@gmail.com>
2020-08-05 10:28:54 +0530

    Dijstra v2

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import React, { FC, useRef } from "react";
 import "./App.css";
 import DrawGrid from "./components/drawGrid";
 
+interface q {
+  index: number;
+  distance: number;
+}
+
 const App: FC = () => {
   const obstacles = useRef<number[]>([]);
   const squareWidth = useRef<number>(10);
@@ -17,11 +22,12 @@ const App: FC = () => {
   const end_node = useRef<number>(0);
 
   // checks if the given index is valid
-  function inBounds(index: number): boolean {
+  let inBounds = (index: number): boolean => {
     return index >= 0 && index <= numberOfSquares.current;
-  }
+  };
 
-  function getAdjacentNodes(index: number): number[] {
+  // given a vertex returns the top, left, right and bottom vertices
+  let getAdjacentNodes = (index: number): number[] => {
     let nRow: number = gridWrapperWidth.current / squareWidth.current;
     let adjList: number[] = [];
     let top: number = inBounds(index - nRow) ? index - nRow : -1;
@@ -33,16 +39,42 @@ const App: FC = () => {
     if (index % nRow !== 0) adjList.push(index - 1); // left
 
     return adjList;
-  }
+  };
 
   // checks if there is a obstacle in the given index
-  function isObstacle(index: number): boolean {
+  let isObstacle = (index: number): boolean => {
     let isObs: boolean = false;
     obstacles.current.forEach((obstacle: number) => {
       if (Number(obstacle) === index) isObs = true;
     });
     return isObs;
-  }
+  };
+
+  // inserts elements such that the sorted order is maintained (sorted by distance)
+  let insert = (queue: q[], { index, distance }: q): q[] => {
+    if (queue.length === 0 || queue.length === 1) {
+      queue.push({ index, distance });
+      return queue;
+    }
+
+    let added: boolean = false;
+    for (let i = 0; i < queue.length; i++) {
+      if (distance < queue[i].distance) {
+        added = true;
+        queue.splice(i, 0, { index, distance });
+        break;
+      }
+    }
+
+    if (!added) queue.push({ index, distance });
+
+    return queue;
+  };
+
+  // gets the distance from the start node
+  let getDistance = (index: number): number => {
+    return Math.abs(start_node.current - index);
+  };
 
   // main function for Dijstra's shortest path
   function Dijstra() {
@@ -53,16 +85,20 @@ const App: FC = () => {
     let visited: boolean[] = [];
 
     for (let i = 0; i < numberOfSquares.current; i++) visited.push(false);
-
     queue.push(start_node.current);
-    let running: boolean = true;
 
-    while (running) {
+    let main = () => {
       let currentIndex: number = queue[0];
-      if (currentIndex === end_node.current) running = false;
+      if (currentIndex === end_node.current) {
+        backTrack();
+        return;
+      }
       // getting the adjacent elements of current node
       adjList = getAdjacentNodes(currentIndex);
       console.log("adjacent list of ", currentIndex + ": ");
+      let element = document.getElementById(currentIndex.toString());
+      if (element !== null && currentIndex !== start_node.current)
+        element.style.backgroundColor = "cyan";
       // eslint-disable-next-line no-loop-func
       adjList.forEach((adjIndex: number) => {
         if (adjIndex !== -1 && !visited[adjIndex] && !isObstacle(adjIndex)) {
@@ -79,15 +115,23 @@ const App: FC = () => {
       });
       visited[currentIndex] = true;
       queue.shift();
-    }
+      setInterval(() => {
+        main();
+      }, 500);
+    };
 
-    // back tracking from the end_node
-    let i: number | undefined = prevNodes.get(end_node.current);
-    while (i !== start_node.current && i !== undefined) {
-      let e = document.getElementById(i.toString());
-      if (e !== null) e.style.backgroundColor = "yellow";
-      i = prevNodes.get(i);
-    }
+    let backTrack = () => {
+      console.log("PrevNodes : " + prevNodes);
+      // back tracking from the end_node
+      let i: number | undefined = prevNodes.get(end_node.current);
+      while (i !== start_node.current && i !== undefined) {
+        let e = document.getElementById(i.toString());
+        if (e !== null) e.style.backgroundColor = "yellow";
+        i = prevNodes.get(i);
+      }
+    };
+
+    main();
   }
 
   return (
